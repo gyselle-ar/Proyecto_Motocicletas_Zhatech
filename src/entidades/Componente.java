425a89c6465adef02c6431285e51c027c976e88b
gyselle.ar <gyselle.ar@gmail.com>
2026-05-20 01:18:55 -0500

    feat: Corregimos lógica de algunas clases y eliminamos otras.

--- src/entidades/Componente.java
+++ src/entidades/Componente.java
@@ -6,38 +6,38 @@ public class Componente {
     private int idComponente;
     private String nombre;
     private String categoria;
+    private TipoMoto tipoMoto;
+    private Marca marca;
 
     public Componente() {
     }
 
-    public Componente(int idComponente, String nombre, String categoria) {
+    public Componente(int idComponente, String nombre, String categoria, TipoMoto tipoMoto, Marca marca) {
         this.idComponente = idComponente;
         this.nombre = nombre;
         this.categoria = categoria;
+        this.tipoMoto = tipoMoto;
+        this.marca = marca;
     }
 
     public int getIdComponente() {
         return idComponente;
     }
 
-    public void setIdComponente(int idComponente) {
-        this.idComponente = idComponente;
-    }
-
     public String getNombre() {
         return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
     }
 
     public String getCategoria() {
         return categoria;
     }
 
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
+    public TipoMoto getTipoMoto() {
+        return tipoMoto;
+    }
+
+    public Marca getMarca() {
+        return marca;
     }
 
     @Override
